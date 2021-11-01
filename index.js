04b354e0bc74c64ac4937a6bd5884d58e1bb312d
Joslyn <joslynsipod@gmail.com>
2021-10-31 20:32:05 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction(Josh);
+introduction(Nancy);
+
+function introductionWithLanguage(name, language) {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguage(Josh, Ember.js));
+console.log(introductionWithLanguage(Nancy, React));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+} console.log(introductionWithLanguageOptional(Gracie, JavaScript));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+} console.log(introductionWithLanguageOptional(Gracie, JavaScript));
